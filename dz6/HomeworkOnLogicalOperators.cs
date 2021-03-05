4c4d19e1230a11cc848a17e3f161ed583988b90c
Nebo <kindersmail08@gmail.com>
2021-03-05 14:39:52 +0300

    добавил исключение в класс HomeworkOnVariables

---
+++ dz6/HomeworkOnLogicalOperators.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dz6
+{
+    public static class HomeworkOnLogicalOperators
+    {
+        public static int ComparingTwoNumbers (int a, int b)
+        {
+            //int a = 0;
+            //int b = 0;
+            //Console.WriteLine("Введи A B C");
+            //a = Convert.ToInt32(Console.ReadLine());
+            //b = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+
+            if (a > b)
+            {
+                //Console.WriteLine($"A+B = {a + b}");
+                result = a + b;
+            }
+            else if (a == b)
+            {
+                //Console.WriteLine($"A*B = {a * b}");
+                result = a * b;
+            }
+            else
+            {
+                //Console.WriteLine($"a-b = {a - b}");
+                result = a - b;
+            }
+            return result;
+        }
+
+        public static string SelectionQuarter (int x, int y)
+        {
+            //int x = 0;
+            //int y = 0;
+            //Console.WriteLine("Введи X и Y");
+            //x = Convert.ToInt32(Console.ReadLine());
+            //y = Convert.ToInt32(Console.ReadLine());
+            string result = "";
+
+            if ((x == 0) && (y == 0))
+            {
+                result = "олимпиадник ввел нули";
+                //Console.WriteLine("олимпиадник ввел нули");
+            }
+            else if (x >= 0)
+            {
+                if (y >= 0)
+                {
+                    result = "I четверть";
+                    //Console.WriteLine("I четверть");
+
+                }
+                else
+                {
+                    result = "III четверть";
+                    //Console.WriteLine("III четверть");
+                }
+            }
+            else
+            {
+                if (y >= 0)
+                {
+                    result = "II Четверть";
+                    //Console.WriteLine("II Четверть");
+                }
+                else
+                {
+                    result = "IV Четверть";
+                    //Console.WriteLine("IV Четверть");
+                }
+            }
+            return result;
+        }
+
+
+    }
+}
---
+++ dz6/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace dz6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int homeWorkNumber = 0;
+            int taskNumber = 0;
+            Console.WriteLine("Введи номер домашнего задания 1-4");
+            homeWorkNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введи номер задачи");
+            taskNumber = Convert.ToInt32(Console.ReadLine());
+            switch (homeWorkNumber)
+            {
+                case 1:
+                    switch (taskNumber)
+                    {
+                        case 1:
+                            double resultDouble = 0;
+                            resultDouble = HomeworkOnVariables.SolutionOfAGivenEquation(5, 10);
+                            break;
+                        case 2:
+                            double[] resultArray = new double[2];
+                            resultArray = HomeworkOnVariables.ChangeOfPlacesOfVariables(2, 10);
+                            break;
+                        case 3:
+                            int[] resultArray1 = new int[2];
+                            resultArray1 = HomeworkOnVariables.DivisionResult(2, 10);
+                            break;
+                        case 4:
+                            double resultDouble1 = 0;
+                            resultDouble1 = HomeworkOnVariables.SolutionLinearEquation(1, 2, 3);
+                            break;
+                        case 5:
+                            string resultString;
+                            resultString = HomeworkOnVariables.DescriptionOfTheEquationPassingThroughtThePoints(1, 2, 3, 4);
+                            Console.WriteLine(resultString);
+                            break;
+
+
+
+                    }
+
+                    break;
+                case 2:
+                    switch (taskNumber)
+                    {
+                        case 1:
+                            int resultInt = 0;
+                            resultInt = HomeworkOnLogicalOperators.ComparingTwoNumbers(5, 10);
+                            break;
+                        case 2:
+                            string resultString = "";
+                            resultString = HomeworkOnLogicalOperators.SelectionQuarter(1, -2);
+                            break;
+                    }
+
+                    break;
+                case 3:
+                    switch (taskNumber)
+                    {
+                        case 1:
+                            int resultInt = 0;
+                            resultInt = HomeworkOnCycles.Exponention(2, 2);
+                            break;
+                        case 2:
+                            int a = 1;
+                            int[] resultArray = new int[1000 / a];
+                            resultArray = HomeworkOnCycles.DivisionByNymber(a);
+                            break;
+                    }
+
+                    break;
+                case 4:
+                    switch (taskNumber)
+                    {
+                        case 1:
+                            int resultInt = 0;
+                            int[] array = new int[6] { 4, 8, 15, 16, 23, 42 };
+                            resultInt = HomeworkOnOneArrays.FindTheMinimumElementOfAnArray(array);
+                            break;
+                        case 2:
+                            int resultInt1 = 0;
+                            int[] array1 = new int[6] { 4, 8, 15, 16, 23, 42 };
+                            resultInt1 = HomeworkOnOneArrays.FindTheMaximumElementOfAnArray(array1);
+                            break;
+                    }
+
+                    break;
+            }
+        }
+    }
+}
