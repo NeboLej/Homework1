4c223e55711024983d59ed383d5199836dcb8894
Nebo <kindersmail08@gmail.com>
2021-03-05 14:39:52 +0300

    initial

---
+++ dz6/HomeworkOnCycles.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dz6
+{
+    public static class HomeworkOnCycles
+    {
+        public static int Exponention (int a, int b)
+        {
+            //int a = 0;
+            //int b = 0;
+            //a = Convert.ToInt32(Console.ReadLine());
+            //b = Convert.ToInt32(Console.ReadLine());
+            if (b == 0)
+            {
+                return 1;
+            }
+            int a0 = a;
+            for (int i = 2; i<=b; i ++)
+            {
+                a *= a0;
+
+            }
+            Console.WriteLine($"{a0} в степени {b} = {a}");
+            return a;
+        }
+        public static int[] DivisionByNymber (int a)
+        {
+            int[] result = new int[1000 / a];
+            int j = 0;
+            //int a = 0;
+            //a = Convert.ToInt32(Console.ReadLine());
+            for(int i = a; i<=1000; i += a)
+            {
+                j += 1;
+                result[j] = i;
+
+            //    Console.WriteLine(i);
+            }
+            return result;
+        }
+
+        //public static int FindTheNumberWhoseSquareIsLess(int a)
+        //{
+        //    // int a = 0;
+        //    //a = Convert.ToInt32(Console.ReadLine());
+        //    for(int i = a; i<=1000; i += a)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //    return; 
+        //}
+    }
+}
---
+++ dz6/HomeworkOnOneArrays.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dz6
+{
+    public static class HomeworkOnOneArrays
+    {
+        public static int FindTheMinimumElementOfAnArray(int[] array)
+        {
+            int minElem = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < minElem)
+                {
+                    minElem = array[i];
+                }
+            }
+            //Console.WriteLine(minElem);
+            return minElem;
+        }
+
+
+
+        public static int FindTheMaximumElementOfAnArray(int[] array)
+        {
+            int maxElem = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > maxElem)
+                {
+                    maxElem = array[i];
+                }
+            }
+            //Console.WriteLine(maxElem);
+            return maxElem;
+        }
+
+
+
+
+    }
+}
