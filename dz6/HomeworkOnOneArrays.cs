18c937d5fcb7ac49b3eca3f411e0481997ee3d1c
Nebo <kindersmail08@gmail.com>
2021-03-08 23:09:58 +0300

    task 3-6 class HomeworkOnOneArrays

--- dz6/HomeworkOnOneArrays.cs
+++ dz6/HomeworkOnOneArrays.cs
@@ -20,8 +20,6 @@ namespace dz6
             return minElem;
         }
 
-
-
         public static int FindTheMaximumElementOfAnArray(int[] array)
         {
             int maxElem = array[0];
@@ -35,6 +33,74 @@ namespace dz6
             //Console.WriteLine(maxElem);
             return maxElem;
         }
+
+        public static int FindTheIndexMinimumElement(int[] array)
+        {
+            int minElem = array[0];
+            int minIndex = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < minElem)
+                {
+                    minElem = array[i];
+                    minIndex = i;
+                }
+            }
+            return minIndex;
+            //Console.WriteLine(minIndex);
+        }
+
+        public static int FindTheIndexMaximumElement(int[] array)
+        {
+            int maxElem = array[0];
+            int maxIndex = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > maxElem)
+                {
+                    maxElem = array[i];
+                    maxIndex = i;
+                }
+            }
+            return maxIndex;
+            //Console.WriteLine(maxIndex);
+        }
+
+        public static int CalculatingSumItemsOddIndex(int[] array)
+        {
+            int sumElem = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            for (int i = 1; i < array.Length; i += 2)
+            {
+                sumElem += array[i];
+            }
+            return sumElem;
+            //Console.WriteLine();
+            //Console.WriteLine("сумма чмсел с нечетными индексами = " + sumElem);
+        }
+
+        public static int[] ReverseArray(int[] array)
+        {
+
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                int tmp = array[i];
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = tmp;
+
+            }
+            return array;
+
+        }
+
+
+
+
+
 
 
 
