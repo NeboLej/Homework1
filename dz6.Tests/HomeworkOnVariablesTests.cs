d192bcc6e126abc7a5c8054de8c1752f4ff27c47
Nebo <kindersmail08@gmail.com>
2021-03-09 13:25:35 +0300

    added testing of 1 method class HomeworkOnvariables

---
+++ dz6.Tests/HomeworkOnVariablesTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+
+namespace dz6.Tests
+{
+    class HomeworkOnVariablesTests
+    {
+        [TestCase(5, 4, -41)]
+        [TestCase(0, 1, 1)]
+        [TestCase(0.5d, 0.1d, -6.275d)]
+        public void SolutionTest(double a, double b, double expected)
+        {
+            double actual = HomeworkOnVariables.SolutionOfAGivenEquation(a, b);
+
+            Assert.AreEqual(expected, actual,0.00001);
+        }
+
+    }
+}
