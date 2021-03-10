d122fb1cdf5dd24b9472513e86409fa2521c3641
Nebo <kindersmail08@gmail.com>
2021-03-10 19:32:48 +0300

    completed decision HomeworkOnTwoArrays

---
+++ dz6/HomeworkOnTwoArrays.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dz6
+{
+    public static class HomeworkOnTwoArrays
+    {
+        public static int FindMimimalElement(int[,] array)
+        {
+            int minElement = array[0,0];
+            for (int i = 0; i != array.GetLength(0); i++)
+            {
+                for (int j  = 0; j != array.GetLength(1); j++)
+                {
+                    if (array[i, j] < minElement)
+                    {
+                        minElement = array[i, j];
+                    }
+                }
+            }
+            return minElement;
+        }
+
+        public static int FindMaximalElement(int[,] array)
+        {
+            int maxElement = array[0,0];
+            for (int i = 0; i != array.GetLength(0); i++)
+            {
+                for (int j  = 0; j != array.GetLength(1); j++)
+                {
+                    if (array[i, j] > maxElement)
+                    {
+                        maxElement = array[i, j];
+                    }
+                }
+            }
+            return maxElement;
+        }
+
+        public static int[] FindIndexMimimalElement(int[,] array)
+        {
+            int iMinimal = 0;
+            int jMinimal = 0;
+            int minElement = array[iMinimal, jMinimal];
+
+            for (int i = 0; i != array.GetLength(0); i++)
+            {
+                for (int j = 0; j != array.GetLength(1); j++)
+                {
+                    if (array[i, j] < minElement)
+                    {
+                        iMinimal = i;
+                        jMinimal = j;
+                        minElement = array[iMinimal, jMinimal];
+                    }
+                }
+            }
+            int[] result = new int[] { iMinimal, jMinimal };
+            return result;
+
+
+        }     
+        
+        public static int[] FindIndexMaximalElement(int[,] array)
+        {
+            int iMax = 0;
+            int jMax = 0;
+            int MaxElement = array[iMax, jMax];
+
+            for (int i = 0; i != array.GetLength(0); i++)
+            {
+                for (int j = 0; j != array.GetLength(1); j++)
+                {
+                    if (array[i, j] > MaxElement)
+                    {
+                        iMax = i;
+                        jMax = j;
+                        MaxElement = array[iMax, jMax];
+                    }
+                }
+            }
+            int[] result = new int[] { iMax, jMax };
+            return result;
+        }
+
+        public static int CountNumberElemMoreNeighbors(int[,] array)
+        {
+            int count = 0;
+            for(int i = 0; i<array.GetLength(0); i++)
+            {
+                for(int j = 0; j<array.GetLength(1); j++)
+                {
+                    if((i<=0 || array[i,j] > array[i-1, j])
+                        && (i >= array.GetLength(0)-1 || array[i,j]>array[i+1,j])
+                        && (j <= 0 || array[i, j]>array[i, j-1])
+                        && (j >= array.GetLength(1)-1 || array[i,j]>array[i, j + 1]))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public static int[,] ReflectAnArray(int[,] array)
+        {
+            int[,] resultArray = new int[array.GetLength(1), array.GetLength(0)];
+
+            for (int i = 0; i<array.GetLength(0); i++)
+            {
+                for(int j=0; j<array.GetLength(1); j++)
+                {
+                    resultArray[j, i] = array[i, j];
+                }
+            }
+            return resultArray;
+        }
+
+
+    }
+}
--- dz6/Program.cs
+++ dz6/Program.cs
@@ -6,7 +6,61 @@ namespace dz6
     {
         static void Main(string[] args)
         {
-            HomeworkOnCycles.FindTheNumberOfTheFibonacciSeries(5);
+            //int result = 0;
+            //int[,] array = new int[,] { { 3, 5, 7 }, { 8, -3, -9 }, { 0, 18, 90 } };
+            //result = HomeworkOnTwoArrays.FindMimimalElement(array);
+            //Console.WriteLine(result);
+
+            //int result = 0;
+            //int[,] array = new int[,] { { 3, 5, 700 }, { 8, -3, -9 }, { 0, 18, 90 } };
+            //result = HomeworkOnTwoArrays.FindMaximalElement(array);
+            //Console.WriteLine(result);
+
+
+            //int[] result = new int[2];
+            //int[,] array = new int[,] { { 3, 5, -700 }, { 8, -3, -9 }, { 0, 18, 90 } };
+            //result = HomeworkOnTwoArrays.FindIndexMimimalElement(array);
+            //Console.WriteLine(result[0]);
+            //Console.WriteLine(result[1]);
+
+
+            //int[] result = new int[2];
+            //int[,] array = new int[,] { { 3, 5, 700 }, { 8, -3, -9 }, { 0, 18, 90 } };
+            //result = HomeworkOnTwoArrays.FindIndexMaximalElement(array);
+            //Console.WriteLine(result[0]);
+            //Console.WriteLine(result[1]);
+
+
+            //int result = 0;
+            //int[,] array = new int[,] { { 3, 5, 700 }, { 8, 9, -9 }, { 0, 18, 90 } };
+            //result = HomeworkOnTwoArrays.CountNumberElemMoreNeighbors(array);
+            //Console.WriteLine(result); 
+            
+            int[,] array = new int[,] { { 3, 5, 700 }, { 8, 9, -9 }, { 0, 18, 90 } };
+            int[,] resultArray = new int[array.GetLength(1), array.GetLength(0)];
+            resultArray = HomeworkOnTwoArrays.ReflectAnArray(array);
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{ array[i, j]} ");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < resultArray.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < resultArray.GetLength(1); j++)
+                {
+                    Console.Write($"{ resultArray[i, j]} ");
+                }
+            }
+
+
         }
     }
 }
